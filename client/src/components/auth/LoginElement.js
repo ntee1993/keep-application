6262ae4526baa6588f862e25479bf53e1bdf6964
Teerayoot Boonsai <n.teerayoot@gmail.com>
2020-10-15 08:56:20 +0700

    login auth config
    with no after auth action

--- client/src/components/auth/LoginElement.js
+++ client/src/components/auth/LoginElement.js
@@ -1,6 +1,16 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 import { TextField, Button } from "@material-ui/core";
+import PropTypes from "prop-types";
+import { login } from "../../actions/authActions";
+import { clearErrors } from "../../actions/errorActions";
+import { connect } from "react-redux";
+
+const init = {
+  email: "",
+  password: "",
+  msg: null,
+};
 
 const theme = createMuiTheme({
   palette: {
@@ -10,31 +20,72 @@ const theme = createMuiTheme({
   },
 });
 
-const LoginElement = () => {
+const LoginElement = ({ isAuthenticated, error, login, clearErrors }) => {
+  const [state, setState] = useState(init);
+
+  useEffect(() => {
+    // Check for login error
+    if (error.id === "LOGIN_FAIL") {
+      setState({
+        ...state,
+        msg: error.msg.msg,
+      });
+    } else {
+      setState({
+        ...state,
+        msg: null,
+      });
+    }
+  }, [error]);
+
+  const onChange = (e) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const { email, password } = state;
+
+    const user = {
+      email,
+      password
+    };
+
+    login(user)
+  };
+
   return (
     <Fragment>
-      <form className="mt-5">
+      <form className="mt-5" onSubmit={onSubmit}>
         <h1 align="center">KEEP Application</h1>
         <br></br>
         <ThemeProvider theme={theme}>
           <div className="mb-3">
             <TextField
+              name="email"
+              type="email"
               id="outlined-basic"
               label="Email"
               variant="outlined"
-              type="email"
+              onChange={onChange}
             />
           </div>
           <div>
             <TextField
+              name="password"
+              type="password"
               id="outlined-password-input"
               label="Password"
               variant="outlined"
-              type="password"
+              onChange={onChange}
             />
           </div>
           <div className="mt-3" align="center">
-            <Button className="login-btn" variant="contained" color="primary">
+            <Button type="submit" className="login-btn" variant="contained" color="primary">
               <strong>LOG IN</strong>
             </Button>
           </div>
@@ -44,4 +95,16 @@ const LoginElement = () => {
   );
 };
 
-export default LoginElement;
+LoginElement.propTypes = {
+  isAuthenticated: PropTypes.bool,
+  error: PropTypes.object.isRequired,
+  login: PropTypes.func.isRequired,
+  clearErrors: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated,
+  error: state.error,
+});
+
+export default connect(mapStateToProps, { login, clearErrors })(LoginElement);
